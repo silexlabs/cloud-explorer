253ee524afb933e7f3ec13d0c44a487603513048
Alex Hoyau <a.hoyau@silexlabs.org>
2013-05-15 18:28:57 +0200

    target unifile.herokuapp.com

--- app/js/config.js
+++ app/js/config.js
@@ -7,6 +7,6 @@ CEConfig = {
   /**
    * Server URL, uses the unifile server which is a node server
    */
-  serverUrl : 'http://localhost:5000\\:5000/v1.0/',
-  //serverUrl : 'http://unifile.herokuapp.com/v1.0/',
+  //serverUrl : 'http://localhost:5000\\:5000/v1.0/',
+  serverUrl : 'http://unifile.herokuapp.com/v1.0/',
 }
