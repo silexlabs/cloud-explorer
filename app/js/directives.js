9c126d8499a2da47d55f2f369fa146897ef23a48
Thomas Fetiveau <thomas.fetiveau.tech@gmail.com>
2013-05-27 18:06:45 +0200

    Add drag n' drop between files and from desktop add few fixes and cleanup

--- app/js/directives.js
+++ app/js/directives.js
@@ -18,6 +18,8 @@
  * drag 'n drop
  *
  * bootstrap styling
+ * 
+ * refresh after upload !!!!
  */
 
 /* Config */
@@ -35,11 +37,213 @@ angular.module('ceConf', [])
 	}]);
 
 
+/* Services */
+angular.module('ceServices', ['ngResource', 'ceConf'])
+
+	.factory('ceConsoleSrv', [ '$rootScope', 'console.level', function( $rootScope, level )
+	{
+		return { 
+			"log": function( msg, l ) { if ( l >= level ) $rootScope.$emit("log", msg, l); }
+		};
+	}])
+
+	.factory('$unifileSrv', ['$resource', 'server.url', function( $resource, serverUrl )
+	{
+		//return $resource(CEConfig.serverUrl+':service/:method/:command/?:path/', {}, {  // workaround: "?" is to keep a "/" at the end of the URL
+		return $resource( serverUrl + ':service/:method/:command/:path ', {},
+			{  // *very ugly* FIXME added space to keep the '/' at the end of the url
+				listServices: {method:'GET', params:{service:'services', method:'list'}, isArray:true},
+				connect: {method:'GET', params:{method:'connect'}, isArray:false},
+				login: {method:'GET', params:{method:'login'}, isArray:false},
+				ls: {method:'GET', params:{method:'exec', command:'ls'}, isArray:true},
+				rm: {method:'GET', params:{method:'exec', command:'rm'}, isArray:false},
+				mkdir: {method:'GET', params:{method:'exec', command:'mkdir'}, isArray:false},
+				cp: {method:'GET', params:{method:'exec', command:'cp'}, isArray:false},
+				mv: {method:'GET', params:{method:'exec', command:'mv'}, isArray:false},
+				//get: {method:'GET', params:{method:'exec', command:'get'}, isArray:true}
+			});
+	}])
+
+	.service('$fileUpload', ['$http', 'ceConsoleSrv', function($http, ceConsoleSrv)
+	{
+		this.upload = function(uploadFiles, path)
+		{
+			//Not really sure why we have to use FormData().  Oh yeah, browsers suck.
+			var formData = new FormData();
+			for(var i in uploadFiles)
+			{
+				formData.append('data', uploadFiles[i], uploadFiles[i].name);
+			}
+console.log(formData);
+			$http({
+					method: 'POST',
+					url: 'http://127.0.0.1:5000/v1.0/dropbox/exec/put/' + path + uploadFiles[0].name,
+					data: formData,
+					headers: {'Content-Type': undefined},
+					transformRequest: angular.identity
+				})
+				.success(function(data, status, headers, config) {
+					ceConsoleSrv.log("file(s) successfully sent", 0);
+				});
+		}
+	}]);
+
+
 /* Controllers */
-angular.module('ceCtrls', [])
+angular.module('ceCtrls', ['ceServices'])
+
+	/**
+	 * This controller is shared by the ceFile and ceFolder directives. This allows them to share the same scope as 
+	 * sharing an isolated scope between directives doesn't seem to be allowed.
+	 */
+	.controller('CEFileEntryCtrl', ['$scope', '$element', '$attrs', '$transclude', '$fileUpload', '$unifileSrv', function($scope, $element, $attrs, $transclude, $fileUpload, $unifileSrv)
+		{
+			$scope.filePath = $scope.path; console.log('$scope.filePath= '+$scope.filePath);
+			$scope.fileSrv = $scope.srv; console.log('$scope.fileSrv= '+$scope.fileSrv);
+
+			if ($scope.file != null)
+			{
+				$scope.filePath += $scope.file.name; console.log('finally $scope.filePath= '+$scope.filePath);
+			}
+
+			/**
+			 * TODO comment
+			 */
+			$scope.setLinkToParent = function()
+			{
+				$scope.$watch('path', function() {
+					if ( $scope.path != undefined && $scope.path != '' && $scope.path != '/' )
+					{
+						var p = $scope.path;
+						if (p.lastIndexOf('/') == p.length-1) p = p.substr(0, p.length-1);
+						$scope.filePath = p.substr(0, p.lastIndexOf('/'));
+					}
+				});
+			}
+			/**
+			 * TODO comment
+			 */
+			$scope.isRootPath = function()
+			{
+//console.log('isRootPath '+$scope.path);
+				if ( $scope.path == undefined || $scope.path == '' || $scope.path == '/' )
+				{
+//console.log('isRootPath true');
+					return true;
+				}
+//console.log('isRootPath false');
+				return false;
+			};
+
+			/**
+			 * TODO comment
+			 */
+			$scope.enterDir = function()
+			{
+				if ($scope.file != null && $scope.file.is_dir || $scope.file == null)
+				{
+					$scope.cd($scope.filePath, $scope.fileSrv);
+					$scope.ls();
+				}
+				/*else TODO ?
+				{
+					// get the file
+					var filePopup = $window.open( serverUrl + $scope.srv+'/exec/get/'+path, 'filePopup', 'height=800,width=800');
+					filePopup.owner = $window;
+					if ($window.focus) { filePopup.focus(); }
+				}*/
+			}
+
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDragStart = function(e)
+			{
+console.log("ceFile => dragStart,  e.target= "+e.target+",  path= "+$scope.filePath);
+				e.dataTransfer.effectAllowed = 'move';
+				e.dataTransfer.setData('text', $scope.filePath);
+
+				$element.addClass("ce-file-drag"); // FIXME make it a param in conf?
+			}
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDragEnd = function(e)
+			{
+//console.log( "ceFile => dragEnd  file= " + e.dataTransfer.getData('text') );
+				$element.removeClass("ce-file-drag"); // FIXME make it a param in conf?
+			}
+
+			/**
+			 * TODO comment
+			 */
+			$scope.getClass = function()
+			{
+				if ($scope.file != null && !$scope.file.is_dir)
+				{
+					return "is-dir-false";
+				}
+				return "is-dir-true";
+			}
+
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDragEnter = function(e) // TODO manage styles
+			{
+				e.preventDefault();
+//console.log("e.target= "+e.target);
+				$element.addClass("ce-folder-over"); // FIXME make it a param in conf?
+			}
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDragLeave = function(e) // TODO manage styles
+			{
+//console.log("e.target= "+e.target);
+				$element.removeClass("ce-folder-over"); // FIXME make it a param in conf?
+			}
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDragOver = function(e)
+			{
+				if ( e.preventDefault )
+				{
+					e.preventDefault(); // Necessary. Allows us to drop.
+				}
+				e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+				return false;
+			}
+			/**
+			 * TODO comment
+			 */
+			$scope.handleDrop = function(e)
+			{
+console.log("drop ");
+				e.stopPropagation();
+				e.preventDefault();
+				
+				if ( e.dataTransfer.files && e.dataTransfer.files.length > 0 ) // case files from desktop
+				{
+console.log("files from desktop case upload to: " + $scope.filePath);
+					$fileUpload.upload( e.dataTransfer.files, $scope.filePath+'/' );
+				}
+				else // move case
+				{
+					var evPath = e.dataTransfer.getData('text');
+console.log("move " + evPath + " to: " + $scope.filePath+'/'+evPath.substr(evPath.lastIndexOf('/')+1)); // NOTE: new path will probably need to be concatenated with file '/'+name
+//console.log($scope.fileSrv);
+//console.log(evPath+':'+$scope.filePath+'/'+evPath.substr(evPath.lastIndexOf('/')+1));
+					$unifileSrv.mv({service:$scope.fileSrv, path:evPath+':'+$scope.filePath+'/'+evPath.substr(evPath.lastIndexOf('/')+1)});
+				}
+			}
+		}
+	])
 
 	// FIXME can surely be exploded in several specialized ctrls
-	.controller('CEBrowserCtrl', [ '$scope', '$location', '$window', 'ceFile' , 'server.url', 'ceConsoleSrv', function( $scope, $location, $window, ceFile, serverUrl, ceConsole )
+	.controller('CEBrowserCtrl', [ '$scope', '$location', '$window', '$unifileSrv' , 'server.url', 'ceConsoleSrv', function( $scope, $location, $window, $unifileSrv, serverUrl, ceConsole )
 		{
 			function authorize( url )
 			{
@@ -70,7 +274,7 @@ angular.module('ceCtrls', [])
 			function connect( serviceName )
 			{
 				ceConsole.log("Connecting to "+serviceName, 0);
-				var res = ceFile.connect({service:serviceName}, function ()
+				var res = $unifileSrv.connect({service:serviceName}, function ()
 				{
 					ceConsole.log("Connected. Auth url is: "+res.authorize_url, 0);
 
@@ -93,7 +297,8 @@ angular.module('ceCtrls', [])
 			function login()
 			{
 				ceConsole.log("Logging in", 0);
-				var res = ceFile.login({service:$scope.srv}, function (status)
+
+				var res = $unifileSrv.login({service:$scope.srv}, function (status)
 					{
 						ceConsole.log("Login status is: "+status, 0);
 
@@ -122,12 +327,12 @@ angular.module('ceCtrls', [])
 			function listServices()
 			{
 				ceConsole.log("Listing services...", 0);
-				$scope.services = ceFile.listServices();
+				$scope.services = $unifileSrv.listServices();
 			}
 
 			// INITIALIZING
 			$scope.services = [];
-			$scope.connect = connect; // TODO why do I have to do this assignment ?!!!
+			$scope.connect = connect;
 
 			// user status
 			$scope.isLoggedin = false;
@@ -185,6 +390,9 @@ angular.module('ceCtrls', [])
 					throw(Error('Not logged in'));
 				}
 			}
+			// share it to child ctrls
+			$scope.cd = cd;
+
 			/**
 			 * Creates or updates the tree
 			 */
@@ -235,7 +443,7 @@ angular.module('ceCtrls', [])
 console.log( $scope.tree );*/
 				if ($scope.isLoggedin)
 				{
-					var res = ceFile.ls({service:$scope.srv, path:$scope.path}, function (status)
+					var res = $unifileSrv.ls({service:$scope.srv, path:$scope.path}, function (status)
 					{
 						ceConsole.log("Listing command returned "+status, 0);
 /*console.log( 'ls result: ' + res.length + '  scope.tree= ' );
@@ -264,64 +472,8 @@ console.log( $scope.tree );*/
 					throw(Error('Not logged in'));
 				}
 			}
-			/**
-			 * enter directory callback
-			 */
-			$scope.doEnter = function(file, path, srv)
-			{
-//console.log('doEnter file='+file+'  path='+path+'  srv='+srv);
-				if (!path)
-				{
-//console.log('doEnter no path set  path='+path);
-					path = '';
-				}
-				if (!srv)
-				{
-					srv = $scope.srv;
-				}
-				if (file == '' && path == '' || file.is_dir == true)
-				{
-//console.log('doEnter file is a dir');
-					cd(path, srv);
-					ls();
-				}
-				else
-				{
-					// get the file
-					var filePopup = $window.open( serverUrl + $scope.srv+'/exec/get/'+path, 'filePopup', 'height=800,width=800');
-					filePopup.owner = $window;
-					if ($window.focus) { filePopup.focus() }
-				}
-			};
-			$scope.isRootPath = function()
-			{
-//console.log('isRootPath '+$scope.path);
-				if ( $scope.path == '' || $scope.path == '/' )
-				{
-//console.log('isRootPath true');
-					return true;
-				}
-//console.log('isRootPath false');
-				return false;
-			};
-			$scope.enterParentDir = function()
-			{
-//console.log('isRootPath ');
-				if ($scope.path.endsWith('/'))
-				{
-					$scope.path = $scope.path.substr(0, $scope.path.length-1);
-				}
-				var lsi = $scope.path.lastIndexOf('/');
-				if (lsi > 0)
-				{
-					cd('/' + $scope.path.substr(0, lsi));
-				}
-				else
-				{
-					cd('/');
-				}
-				ls();
-			};
+			// share it to child ctrls
+			$scope.ls = ls;
 		}])
 
 	.controller('CEConsoleCtrl', [ '$scope', '$element', function( $scope, $element )
@@ -333,59 +485,6 @@ console.log( $scope.tree );*/
 		}
 		$scope.$on("log", onLogEntry);
 	}]);
-
-
-/* Services */
-angular.module('ceServices', ['ngResource'])
-
-	.factory('ceConsoleSrv', [ '$rootScope', 'console.level', function( $rootScope, level )
-	{
-		return { 
-			"log": function( msg, l ) { if ( l >= level ) $rootScope.$emit("log", msg, l); }
-		};
-	}])
-
-	.factory('ceFile', ['$resource', 'server.url', function( $resource, serverUrl )
-	{
-		//return $resource(CEConfig.serverUrl+':service/:method/:command/?:path/', {}, {  // workaround: "?" is to keep a "/" at the end of the URL
-		return $resource( serverUrl + ':service/:method/:command/:path ', {},
-			{  // *very ugly* FIXME added space to keep the '/' at the end of the url
-				listServices: {method:'GET', params:{service:'services', method:'list'}, isArray:true},
-				connect: {method:'GET', params:{method:'connect'}, isArray:false},
-				login: {method:'GET', params:{method:'login'}, isArray:false},
-				ls: {method:'GET', params:{method:'exec', command:'ls'}, isArray:true},
-				rm: {method:'GET', params:{method:'', command:'rm'}, isArray:false},
-				mkdir: {method:'GET', params:{method:'', command:'mkdir'}, isArray:false},
-				cp: {method:'GET', params:{method:'', command:'cp'}, isArray:false},
-				mv: {method:'GET', params:{method:'', command:'mv'}, isArray:false},
-				//get: {method:'GET', params:{method:'exec', command:'get'}, isArray:true}
-			});
-	}])
-
-	.service('$fileUpload', ['$http', 'ceConsoleSrv', function($http, ceConsoleSrv)
-	{
-		this.upload = function(uploadFiles, path)
-		{
-			//Not really sure why we have to use FormData().  Oh yeah, browsers suck.
-			var formData = new FormData();
-			for(var i in uploadFiles)
-			{
-				formData.append('data', uploadFiles[i], uploadFiles[i].name);
-			}
-console.log(formData);
-			$http({
-					method: 'POST',
-					url: 'http://127.0.0.1:5000/v1.0/dropbox/exec/put/' + path + uploadFiles[0].name,
-					data: formData,
-					headers: {'Content-Type': undefined},
-					transformRequest: angular.identity
-				})
-				.success(function(data, status, headers, config) {
-					ceConsoleSrv.log("file(s) successfully sent", 0);
-				});
-		}
-	}]);
-
 
 /* Directives */
 angular.module('ceDirectives', [ 'ceConf', 'ceServices', 'ceCtrls' ])
@@ -424,6 +523,7 @@ console.log('end change $scope.uploadFiles = '+$scope.uploadFiles);
 		};
 	})
 
+	// this is the CE browser log console
 	.directive('ceConsole', function()
 	{
 		return {
@@ -437,142 +537,36 @@ console.log('end change $scope.uploadFiles = '+$scope.uploadFiles);
 	// this directive implements the behavior of receiving a file/folder on drop
 	.directive('ceFolder', function()
 	{
-		function handleDragEnter(e, dropStyle) // TODO manage styles
-		{
-			e.preventDefault();
-console.log("e.target= "+e.target);
-			//e.target.addClass(dropStyle);
-		}
-		function handleDragLeave(e, dropStyle) // TODO manage styles
-		{
-console.log("e.target= "+e.target);
-			//e.target.removeClass(dropStyle);
-		}
-		function handleDragOver(e)
-		{
-			if ( e.preventDefault )
-			{
-				e.preventDefault(); // Necessary. Allows us to drop.
-			}
-			e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-			return false;
-		}
-		function handleDrop(e)
-		{
-console.log("drop ");
-			e.stopPropagation();
-			e.preventDefault();
-			
-			if ( e.dataTransfer.files && e.dataTransfer.files.length > 0 ) // files from desktop case
-			{
-console.log("files from desktop case ");
-				var files = e.dataTransfer.files;
-				for (var i = 0, f; f = files[i]; i++)
-				{
-					console.log("please upload "+f.name+" to ");
-				}
-			}
-			else // move case
-			{
-console.log("files from CE ");
-				// TODO perform a move
-				console.log("please move " + JSON.parse(e.dataTransfer.getData('application/json')).name + " to "   );
-			}
-		}
 		return {
 			restrict: 'A',
 			link: function(scope, element)
 			{
-				element.bind('dragenter', function(e)
-				{
-console.log("dragenter");
-					handleDragEnter(e, "ce-folder-over"); // FIXME style as a param
-				});
-				element.bind('dragleave', function(e)
-				{
-console.log("dragleave");
-					handleDragLeave(e, "ce-folder-over"); // FIXME style as a param
-				});
-				element.bind('dragover', function(e)
-				{ 
-//console.log("dragover");
-					handleDragOver(e);
-				});
-				element.bind('drop', function(e)
-				{ 
-					handleDrop(e);
-				});
-			}
+				element.bind('dragenter', scope.handleDragEnter );
+				element.bind('dragleave', scope.handleDragLeave );
+				element.bind('dragover', scope.handleDragOver );
+				element.bind('drop', scope.handleDrop );
+			},
+			controller: 'CEFileEntryCtrl'
 		};
 	})
 
 	// this directive implements the behavior of mooving a file on drag
 	.directive('ceFile', function()
 	{
-		function handleDragStart(e, element, dragStyle, file)
-		{
-console.log("ceFile => dragStart,  e.target="+e.target+",  name="+file.name);
-			element.addClass(dragStyle);
-
-			e.dataTransfer.effectAllowed = 'move';
-			e.dataTransfer.setData('application/json', '{ "name": "'+file.name+'" }');
-			/*element.addClass(dragStyle);
-			evt.dataTransfer.setData("id", evt.target.id);*/
-		}
-		function handleDragEnd(e, element, dragStyle)
-		{
-console.log( "ceFile => dragEnd  file= " + JSON.parse(e.dataTransfer.getData('application/json')).name );
-			element.removeClass(dragStyle);
-		}
-
 		return {
 			restrict: 'A',
-			scope: {
-				file: "=",
-				path: "="
-			},
-			replace: true,
-			template: '<li class="ce-file" ng-class="fileClass(file)" ng-click="doEnter(file, path+file.name)">{{file.name}}</li>',
 			link: function(scope, element, attrs)
 			{
-				scope.fileClass = function( file )
-				{
-					if (file.is_dir == true)
-					{
-						return'is-dir-true';
-					}
-					else
-					{
-						return 'is-dir-false';
-					}
-				};
 				attrs.$set('draggable', 'true');
-				
-				//scope.dragData = scope[attrs["drag"]];
-				//scope.dragStyle = attrs["dragstyle"];
-				element.bind('dragstart', function(e)
-				{
-					//$rootScope.draggedElement = scope.dragData;
-					handleDragStart(e, element, "ce-file-drag", scope.file); // FIXME make style a conf param ?
-				});
-				element.bind('dragend', function(e)
-				{
-					handleDragEnd(e, element, "ce-file-drag"); // FIXME make style a conf param ?
-				});
 
-
-				//test to remove
-				element.bind('drop', function(e)
-				{ 
-console.log(" ce file drop");
-					//drop(evt, element, scope.dropStyle);
-					//$rootScope.$broadcast('dropEvent', $rootScope.draggedElement, scope.dropData);
-				});
-			}
+				element.bind('dragstart', scope.handleDragStart );
+				element.bind('dragend', scope.handleDragEnd );
+			},
+			controller: 'CEFileEntryCtrl'
 		};
 	})
 
+	// this is the root directive that you should use in your projects
 	.directive('ceBrowser',  function()
 	{
 		return {
