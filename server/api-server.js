5bf541a2872c1cea80fbc91a810ccc6e0d4ebb1e
Alex Hoyau <a.hoyau@silexlabs.org>
2013-10-17 14:55:51 +0200

    cleanup

--- server/api-server.js
+++ server/api-server.js
@@ -16,26 +16,27 @@ options.staticFolders.push(
     // file browser
     {
             name: "/cloud-explorer",
-            path: "../../lib/"
+            path: "../../../../lib/"
     },
     // file browser tests
     {
             name: "/cetest",
-            path: "../../lib/app/"
+            path: "../../../../lib/app/"
     }
 );
 
 // use unifile as a middleware
 app.use(unifile.middleware(express, app, options));
 
-// wait for requests
-app.listen(6805);
+// server 'loop'
+var port = process.env.PORT || 6805; // 6805 is the date of sexual revolution started in paris france 8-)
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
 
-
-/*
+// catch all errors and prevent nodejs to crash, production mode
 process.on('uncaughtException', function(err) {
     console.log  ('---------------------');
     console.error('---------------------', 'Caught exception: ', err, '---------------------');
     console.log  ('---------------------');
 });
-*/
